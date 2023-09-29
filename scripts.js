9cec3c9578709d801cb119ed973c9fe6820b8225
isaa20066 <146447098+isaa20066@users.noreply.github.com>
2023-09-29 10:57:51 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,27 @@
-// Função para buscar dados da API e exibir imagens ou vídeos com base na busca
 async function fetchMediaFromNASA(searchQuery) {
     try {
-      // Constrói a URL da API da NASA com base na consulta de pesquisa
       const apiUrl = `https://images-api.nasa.gov/search?q=${encodeURIComponent(searchQuery)}`;
       
-      // Faz uma requisição GET à API da NASA para obter os dados da busca
       const response = await fetch(apiUrl);
-      const data = await response.json(); // Converte a resposta para JSON
+      const data = await response.json(); 
   
       const imagesContainer = document.getElementById("images-container");
-      imagesContainer.innerHTML = ""; // Limpar resultados anteriores
+      imagesContainer.innerHTML = ""; 
   
-      // Verifica se os dados recebidos da API são válidos (contêm resultados)
       if (!isValidData(data)) {
         console.error("Nenhum resultado encontrado para a busca.");
         return;
       }
   
-      // Itera sobre cada item da coleção de resultados da API
       for (const item of data.collection.items) {
         const mediaType = item.data[0].media_type;
         
-        // Faz uma nova requisição GET ao link do recurso (imagem ou vídeo)
         const mediaData = await fetch(item.href).then(response => response.json());
         
-        // Obtém o URL correto da mídia (imagem ou vídeo) com base no tipo de mídia
         const mediaUrl = getMediaUrl(mediaData, mediaType);
         const description = item.data[0].description;
         const title = item.data[0].title;
   
-        // Cria o elemento de álbum (imagem ou vídeo) e adiciona-o ao container
         const albumContainer = createAlbumContainer(mediaType, mediaUrl, description, title);
         imagesContainer.appendChild(albumContainer);
       }
@@ -37,22 +29,19 @@ async function fetchMediaFromNASA(searchQuery) {
       console.error("Erro ao buscar dados na API:", error);
     }
   }
-  
-  // Função para verificar se os dados da API são válidos
+
   function isValidData(data) {
     return data && data.collection && data.collection.items && data.collection.items.length > 0;
   }
-  
-  // Função para encontrar o URL correto da mídia (imagem ou vídeo)
+
   function getMediaUrl(mediaData, mediaType) {
     if (mediaType === "image") {
-      return findImageMediaUrl(mediaData); // Encontra o URL correto da imagem (descartando .tif e buscando .jpg ou .png)
+      return findImageMediaUrl(mediaData); 
     } else if (mediaType === "video") {
-      return findVideoMediaUrl(mediaData); // Encontra o URL correto do vídeo (buscando .mp4)
+      return findVideoMediaUrl(mediaData); 
     }
   }
   
-  // Função para encontrar o URL da imagem apropriada (descartar .tif e buscar .jpg ou .png)
   function findImageMediaUrl(mediaData) {
     for (const mediaUrl of mediaData) {
       if (mediaUrl.endsWith(".jpg") || mediaUrl.endsWith(".png")) {
@@ -61,7 +50,6 @@ async function fetchMediaFromNASA(searchQuery) {
     }
   }
   
-  // Função para encontrar o URL do vídeo (buscar .mp4)
   function findVideoMediaUrl(mediaData) {
     for (const mediaUrl of mediaData) {
       if (mediaUrl.endsWith(".mp4")) {
@@ -70,22 +58,18 @@ async function fetchMediaFromNASA(searchQuery) {
     }
   }
   
-  // Função para criar o elemento de álbum (imagem ou vídeo) e adicioná-lo ao container
   function createAlbumContainer(mediaType, mediaUrl, description, title) {
     const albumContainer = document.createElement("div");
     albumContainer.classList.add("album");
   
     if (mediaType === "image") {
-      // Cria o elemento de imagem e adiciona evento de clique para abrir o modal
       const imageElement = createImageElement(mediaUrl, description);
       albumContainer.appendChild(imageElement);
     } else if (mediaType === "video") {
-      // Cria o elemento de vídeo
       const videoElement = createVideoElement(mediaUrl);
       albumContainer.appendChild(videoElement);
     }
   
-    // Adiciona o título como um cabeçalho abaixo da imagem ou vídeo
     const titleElement = document.createElement("h2");
     titleElement.textContent = title;
     albumContainer.appendChild(titleElement);
@@ -93,7 +77,6 @@ async function fetchMediaFromNASA(searchQuery) {
     return albumContainer;
   }
   
-  // Função para criar o elemento de imagem e adicionar evento de clique para abrir o modal
   function createImageElement(mediaUrl, description) {
     const imageElement = document.createElement("img");
     imageElement.src = mediaUrl;
@@ -102,7 +85,6 @@ async function fetchMediaFromNASA(searchQuery) {
     return imageElement;
   }
   
-  // Função para criar o elemento de vídeo
   function createVideoElement(mediaUrl) {
     const videoElement = document.createElement("video");
     videoElement.controls = true;
@@ -113,7 +95,6 @@ async function fetchMediaFromNASA(searchQuery) {
     return videoElement;
   }
   
-  // Função para lidar com o evento de clique no botão de busca
   function handleSearch() {
     const searchInput = document.getElementById("search-input");
     const searchQuery = searchInput.value.trim();
@@ -123,21 +104,18 @@ async function fetchMediaFromNASA(searchQuery) {
     }
   }
   
-  // Adicionar o ouvinte de evento ao botão de busca
   document.getElementById("search-button").addEventListener("click", handleSearch);
   
-  // Função para abrir o modal com a imagem completa e a descrição
   function openModal(mediaUrl, description) {
     const modalContainer = document.getElementById("modal-container");
     const modalImage = document.getElementById("modal-image");
     const modalDescription = document.getElementById("modal-description");
   
     modalImage.src = mediaUrl;
-    modalDescription.textContent = description; // Exibir a descrição no modal
+    modalDescription.textContent = description; 
     modalContainer.style.display = "flex";
   }
   
-  // Função para fechar o modal
   function closeModal() {
     const modalContainer = document.getElementById("modal-container");
     modalContainer.style.display = "none";
